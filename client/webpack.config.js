c38694029b6d22121d5b447e691d13bd4bc5bd1b
venpavel <venpavel@gmail.com>
2023-08-09 21:04:03 +0300

    Client webpack fix for router dom build redirect error

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
         filename: "static/js/[name].[contenthash].js",
         clean: true
     },
